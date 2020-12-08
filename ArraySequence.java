d38b6b2c45cc23b1db44a49e57b1a43f99179d24
Grace <gchen20@stuy.edu>
2020-12-08 12:45:55 -0500

    Tested and implemented first constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,17 +4,30 @@ public class ArraySequence implements IntegerSequence{
   private int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int [] other){
+    data = new int[other.length];
+     for (int i = 0; i < other.length; i++) {
+       data[i] = other[i];
+     }
+  }
 
-  public void reset(){  }
-  public int length(){  }
+  public void reset(){
+
+  }
+  public int length(){
+    return 0;
+  }
 
   //When current is no longer a valid element in the range, it should return false.
-  public boolean hasNext(){  }
+  public boolean hasNext(){
+    return true;
+  }
 
   //@throws NoSuchElementException when hasNext() is false.
   //This will return the current value, it will also increase current value by one index.
   //e.g.  if current is 5. This will make current 6, and return 5.
-  public int next(){    }
+  public int next(){
+    return 0;
+  }
 
 }
--- Tester.java
+++ Tester.java
@@ -1,12 +1,14 @@
 public class Tester {
   public static void main(String[] args) {
-    IntegerSequence r = new Range(10,15);
-    while(r.hasNext()){
-      System.out.print(r.next());
-      if( r.hasNext() ){
-         System.out.print( ", " );
-      }
-    }
-    System.out.println();
+    // IntegerSequence r = new Range(10,15);
+    // while(r.hasNext()){
+    //   System.out.print(r.next());
+    //   if( r.hasNext() ){
+    //      System.out.print( ", " );
+    //   }
+    // }
+    // System.out.println();
+    int[]nums = {1,3,5,0,-1,3,9};
+    IntegerSequence test = new ArraySequence(nums);
   }
 }
